1af924455ca4d36c42dd98a3f9aa5f8abcd23e99
Soul-Xettri <rakeshchhetri420@gmail.com>
2023-09-22 12:07:09 +0545

    Demo component create that changes color theme

---
+++ src/Demo.tsx
@@ -0,0 +1,14 @@
+import { useMantineColorScheme, Button, Group } from '@mantine/core';
+
+export function Demo() {
+  const { setColorScheme, clearColorScheme } = useMantineColorScheme();
+
+  return (
+    <Group style={{height:"1000px"}}>
+      <Button onClick={() => setColorScheme('light')}>Light</Button>
+      <Button onClick={() => setColorScheme('dark')}>Dark</Button>
+      <Button onClick={() => setColorScheme('auto')}>Auto</Button>
+      <Button onClick={clearColorScheme}>Clear</Button>
+    </Group>
+  );
+}
