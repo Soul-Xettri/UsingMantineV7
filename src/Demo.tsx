c07819813cb244136e5a717434d56736d074c2c2
Soul-Xettri <rakeshchhetri420@gmail.com>
2023-09-25 19:34:42 +0545

    removed height from demo

--- src/Demo.tsx
+++ src/Demo.tsx
@@ -4,7 +4,7 @@ export function Demo() {
   const { setColorScheme, clearColorScheme } = useMantineColorScheme();
 
   return (
-    <Group style={{height:"700px"}}>
+    <Group >
       <Button onClick={() => setColorScheme('light')}>Light</Button>
       <Button onClick={() => setColorScheme('dark')}>Dark</Button>
       <Button onClick={() => setColorScheme('auto')}>Auto</Button>
