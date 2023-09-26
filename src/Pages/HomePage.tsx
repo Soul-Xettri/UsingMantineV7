2751933d54ce95af8af08f3530b083001ac468ba
Soul-Xettri <rakeshchhetri420@gmail.com>
2023-09-26 14:16:13 +0545

    homepage fixes

---
+++ src/Pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import BestServices from "../Components/Carasouel/BestServices";
+import { Hero } from "../Components/Hero/Hero";
+import { Demo } from "../Demo";
+
+export default function HomePage() {
+  return (
+    <>
+        <Hero/>
+        <BestServices/>
+        <Demo />
+    </>
+  )
+}
