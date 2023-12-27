20e2a2fac7f9c76db2b2ae77c928e165b0dd4040
sou-xettri <rakeshchhetri420@gmail.com>
2023-12-27 13:11:08 +0545

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,15 @@ import HomePage from "./Pages/HomePage";
 import RoomsPage from "./Pages/RoomsPage";
 import ContactUsPage from "./Pages/ContactUsPage";
 import { Footer } from "./Components/Navbar/Footer";
-
+import { Demo } from "./Pages/Demo";
+import '@mantine/carousel/styles.css';
 function App() {
   return (
     <>
       <MantineProvider theme={theme}>
         <Router>
           <HeaderMegaMenu />
+          <Demo/>
           <Routes>
             <Route path="/">
               <Route index element={<HomePage />} />
